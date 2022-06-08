fd1119abb7175e97b42ec3fd095644b06c00119f
Nikos Vlachakis <nvlachakis1999@gmail.com>
2022-06-08 14:18:06 +0300

    changing pf to ff names

--- ff-send-data/src/index.js
+++ ff-send-data/src/index.js
@@ -10,14 +10,14 @@ app.options("*", cors());
 
 // Import routes
 const helloworld = require("./routes/helloworld");
-const retrievePFData = require("./routes/retrievePFData");
+const retrieveFFData = require("./routes/retrieveFFData");
 const authentication = require("./routes/authentication");
 
 // Tests the connectivity of the server
 app.get("/hello", helloworld);
 
 // Retrieves docs from the database
-app.get("/getPFData", authentication, retrievePFData);
+app.get("/getFFData", authentication, retrieveFFData);
 
 // Server listens to a specific port for incoming requests
 app.listen(port, () => {
